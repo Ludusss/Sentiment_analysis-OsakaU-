976d9c28a7c6891004cd860660427b41e0451968
Stefan Christiansson <stefan.christiansson@senarion.com>
2023-01-04 20:01:42 +0900

    finished feature extraction for ESD data

--- feature_extractor_new.py
+++ feature_extractor_new.py
@@ -214,6 +214,126 @@ def extract_text_features(iemocap_info_df):
 
         return df_features
 
+def extract_ESD_audio_features():
+    emo_dict = {
+        "Angry": 0,
+        "Happy": 1,
+        "Neutral": 2,
+        "Sad": 3,
+        "Surprise": 4
+    }
+    columns = ['set', 'label', 'f0', 'mfcc', 'cqt']
+    df_features = pd.DataFrame(columns=columns)
+    start_sess = 11
+
+    if platform == "darwin":
+        if os.path.isfile("/Users/ludus/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/ESD_audio_features_11.csv"):
+            print("Loaded ESD audio features")
+            df_features = pd.read_csv("extracted_data/ESD/ESD_audio_features_combined.csv")
+            return df_features
+        elif os.path.isfile("/Users/ludus/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/ESD_audio_features_11.csv"):
+            feature_dir = "Users/ludus/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/"
+            features_files = os.listdir(feature_dir)
+            for features_file in features_files:
+                df_features = pd.concat([df_features, pd.read_csv(feature_dir + features_file)])
+                start_sess += 1
+            if start_sess == 21:
+                print("Loaded ESD audio features")
+                return df_features
+    elif platform == "win32":
+        if os.path.isfile("C:/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/ESD_audio_features_combined.csv"):
+            print("Loaded ESD audio features")
+            df_features = pd.read_csv("extracted_data/ESD/ESD_audio_features_combined.csv")
+            return df_features
+        elif os.path.isfile("C:/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/ESD_audio_features_11.csv"):
+            feature_dir = "C:/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/"
+            features_files = os.listdir(feature_dir)
+            for features_file in features_files:
+                df_features = pd.concat([df_features, pd.read_csv(feature_dir + features_file)])
+                start_sess += 1
+            if start_sess == 21:
+                print("Loaded ESD audio features")
+                return df_features
+
+    for sess in tqdm(range(start_sess, 21)):
+        df_features = pd.DataFrame(columns=columns)
+        wav_file_dirs = []
+        wav_ang_val = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Angry/evaluation'.format(sess)
+        wav_ang_test = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Angry/test'.format(sess)
+        wav_ang_train = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Angry/train'.format(sess)
+        wav_hap_val = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Happy/evaluation'.format(sess)
+        wav_hap_test = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Happy/test'.format(sess)
+        wav_hap_train = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Happy/train'.format(sess)
+        wav_neu_val = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Neutral/evaluation'.format(sess)
+        wav_neu_test = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Neutral/test'.format(sess)
+        wav_neu_train = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Neutral/train'.format(sess)
+        wav_sad_val = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Sad/evaluation'.format(sess)
+        wav_sad_test = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Sad/test'.format(sess)
+        wav_sad_train = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Sad/train'.format(sess)
+        wav_sur_val = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Surprise/evaluation'.format(sess)
+        wav_sur_test = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Surprise/test'.format(sess)
+        wav_sur_train = 'raw_data/Emotional Speech Dataset (ESD)/00{}/Surprise/train'.format(sess)
+        wav_file_dirs.append(wav_ang_val)
+        wav_file_dirs.append(wav_ang_test)
+        wav_file_dirs.append(wav_ang_train)
+        wav_file_dirs.append(wav_hap_val)
+        wav_file_dirs.append(wav_hap_test)
+        wav_file_dirs.append(wav_hap_train)
+        wav_file_dirs.append(wav_neu_val)
+        wav_file_dirs.append(wav_neu_test)
+        wav_file_dirs.append(wav_neu_train)
+        wav_file_dirs.append(wav_sad_val)
+        wav_file_dirs.append(wav_sad_test)
+        wav_file_dirs.append(wav_sad_train)
+        wav_file_dirs.append(wav_sur_val)
+        wav_file_dirs.append(wav_sur_test)
+        wav_file_dirs.append(wav_sur_train)
+
+        for wav_file_dir in wav_file_dirs:
+            data_set = "val" if wav_file_dir.split("/")[-1] == "evaluation" else wav_file_dir.split("/")[-1]
+            label = emo_dict[os.path.dirname(wav_file_dir).split("/")[-1]]
+            wav_files = os.listdir(wav_file_dir)
+            for wav_file in tqdm(wav_files):
+                wav_vec, _sr = librosa.load(wav_file_dir + "/" + wav_file, sr=SAMP_RATE)
+                feature_list = [data_set, label]
+
+                # Extract F0-Score
+                f0, _, _ = librosa.pyin(wav_vec, fmin=librosa.note_to_hz('C0'),
+                                        fmax=librosa.note_to_hz('C5'))
+
+                if np.isnan(f0).all():  # If pitch extraction fails discard utterance
+                    continue
+
+                feature_list.append(np.nanmean(f0))
+
+                # Extract mfcc
+                mfcc = librosa.feature.mfcc(y=wav_vec, sr=SAMP_RATE)
+                feature_list.append(np.mean(mfcc, axis=1))
+
+                # Extract Constant-Q chromagram
+                chroma_cq = librosa.feature.chroma_cqt(y=wav_vec, sr=SAMP_RATE, fmin=librosa.note_to_hz('C2'),
+                                                       bins_per_octave=24)
+                feature_list.append(np.mean(chroma_cq, axis=1))
+
+                df_features = pd.concat([df_features, pd.DataFrame(feature_list, index=columns).transpose()])
+
+        df_features.to_csv('extracted_data/ESD/ESD_audio_features_{}.csv'.format(sess), index=False)
+
+    df_features = pd.DataFrame(columns=columns)
+    if platform == "darwin":
+        feature_dir = "Users/ludus/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/"
+        features_files = os.listdir(feature_dir)
+        for features_file in features_files:
+            df_features = pd.concat([df_features, pd.read_csv(feature_dir + features_file)])
+        df_features.to_csv('extracted_data/ESD/ESD_audio_features_combined.csv', index=False)
+    elif platform == "win32":
+        feature_dir = "C:/Projects/Sentiment_analysis-OsakaU-/extracted_data/ESD/"
+        features_files = os.listdir(feature_dir)
+        for features_file in features_files:
+            df_features = pd.concat([df_features, pd.read_csv(feature_dir + features_file)])
+        df_features.to_csv('extracted_data/ESD/ESD_audio_features_combined.csv', index=False)
+
+    return df_features
 
 def main():
     iemocap_df = extract_iemocap_info()
@@ -222,9 +342,10 @@ def main():
     build_audio_vector(iemocap_df)
     audio_features = extract_audio_features(iemocap_df)
     text_features = extract_text_features(iemocap_df)
+    esd_audio_features = extract_ESD_audio_features()
     print("Audio features:\n" + str(audio_features.head()))
     print("Text features:\n" + str(text_features.head()))
-    print(text_features.iloc[0]["b_features"].dtypes)
+    print("ESD audio features:\n" + str(esd_audio_features.head()))
 
 
 if __name__ == '__main__':
